a49771fd203e8ae55c745b99790e8738973d2a5c
Vsevolod <rusakevich.ceva@yandex.ru>
2024-03-07 16:59:23 +0300

    add main page and nav

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<nav>
+    <div class="nav-container">
+        <a href="/" class="nav-logo" title="Вернуться на главную страницу">Всеволод Русакевич</a>
+        <div class="nav-links">
+            <a href="/about" class="link">Обо мне</a>
+            <a href="/block" class="link">Блог</a>
+            <a href="/prodjects" class="link">Портфолио</a>
+            <a href="/contact" class="link">Контакты</a>
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+    <!--Страницы будут добавлены ниже-->
+    <slot></slot>
+</div>
+
+<style>
+    .container {
+        max-width: 1400px;
+        margin: 50px auto;
+    }
+</style>
