835678982de07bab384500d0e8caa78a9e9d551c
Vsevolod <rusakevich.ceva@yandex.ru>
2024-03-07 19:03:49 +0300

    add blog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,23 @@
 <script>
     const nav = [
-        { title: "Обо мне", href: "/about" },
-        { title: "Блог", href: "/blog" },
-        { title: "Портфолио", href: "/project" },
-        { title: "Контакты", href: "/contact" },
-        { title: "temp", href: "/temp" },
+        { title: "Обо мне", href: "/about", isShow: true },
+        { title: "Блог", href: "/blog", isShow: true },
+        { title: "Портфолио", href: "/project", isShow: true },
+        { title: "Контакты", href: "/contact", isShow: true },
+        { title: "temp", href: "/temp", isShow: true },
     ];
 </script>
 
 <nav>
     <div class="nav-container">
-        <a href="/" class="nav-logo" title="Вернуться на главную страницу">Всеволод Русакевич</a>
+        <a href="/" class="nav-logo" title="Вернуться на главную страницу"
+            >Всеволод Русакевич</a
+        >
         <div class="nav-links">
             {#each nav as link}
-                <a href={link.href} class="link">{link.title}</a>
+                {#if link.isShow}
+                    <a href={link.href} class="link">{link.title}</a>
+                {/if}
             {/each}
         </div>
     </div>
---
+++ src/routes/blog/posts/1/+page.svelte
@@ -0,0 +1,49 @@
+<h1>My First Blog Post</h1>
+
+<p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat libero
+    omnis vero esse impedit commodi quasi possimus? Consectetur possimus minima
+    iste veniam, debitis dolore dicta nisi pariatur asperiores molestias
+    architecto enim a perspiciatis expedita! Alias quis modi debitis
+    exercitationem ex fugit eveniet cum minima rem. Ducimus quibusdam numquam
+    molestias enim ex beatae ullam quam ab! Voluptatem inventore pariatur sint
+    eligendi soluta fugit laborum tempore exercitationem culpa. Distinctio
+    laboriosam eius iusto quam nesciunt vel minus nostrum officia. Eos quam
+    accusamus odio error non cumque voluptatum numquam quibusdam sit. Porro
+    aspernatur possimus hic ipsam, adipisci doloremque sequi minima ipsa iusto
+    explicabo velit, praesentium dolorem inventore fuga necessitatibus est
+    repellendus dolorum ullam dolore sit accusamus ducimus quibusdam officia
+    excepturi. Reprehenderit magnam cumque voluptatum, sint harum dolorum quae
+    quisquam aliquam, deleniti praesentium alias accusantium. Quae minus
+    voluptatem, officiis consequuntur aliquid iusto quia rem. Ipsam animi et
+    quod veritatis iste, architecto atque similique a explicabo, labore odit
+    voluptas sit ut distinctio possimus doloremque. A, vero natus deserunt
+    mollitia est amet ducimus minima nam, beatae quidem laudantium ex rerum
+    maiores soluta, eveniet maxime alias illum aperiam aliquid consectetur. Eum
+    laboriosam placeat incidunt molestias magni, fuga saepe dolores commodi
+    optio quaerat. Blanditiis quasi autem corporis quod maxime.
+</p>
+
+<p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate qui
+    totam recusandae nisi sunt quod odit a illum alias veniam laudantium, saepe
+    esse beatae ab eligendi dolorum natus impedit pariatur dignissimos ea? Nulla
+    aut maxime deserunt ratione ut molestiae placeat dolor autem sit veritatis.
+    Blanditiis recusandae ullam nobis alias quasi repudiandae dolores
+    necessitatibus illo quod est beatae modi error cumque fuga libero nemo rem
+    aspernatur, voluptatibus quo, sunt sit. Veniam illo officia ipsa corporis
+    alias iure voluptatibus perferendis fugiat voluptatem repellendus at quam et
+    sed esse velit, eius accusamus nobis repellat unde voluptatum! Nesciunt
+    placeat, consequatur facilis doloremque non pariatur delectus, corrupti
+    rerum necessitatibus dignissimos reiciendis, et est? Debitis hic voluptatum
+    voluptates architecto. Voluptate asperiores temporibus dolore recusandae,
+    nulla nam voluptates suscipit maxime laborum quaerat necessitatibus tempore,
+    dignissimos saepe eveniet ipsam et nobis nisi facilis est quas corporis
+    consectetur! Tenetur totam quis ab voluptatibus minima doloremque similique,
+    non nisi eos quasi illo soluta excepturi quod veritatis harum dicta vel,
+    voluptas beatae fuga quas quia incidunt labore. Vero reprehenderit
+    asperiores ad facere officia iure, debitis fuga quos? Iusto tempora velit
+    veniam modi nostrum, corporis eveniet tenetur deserunt eaque optio. Fugit
+    nulla placeat, laboriosam dignissimos id et in officiis temporibus magnam
+    aperiam.
+</p>
